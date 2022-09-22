7aaa60d9c0dfa3b7c0bf568f1fc140092ef37bc4
dhananjaykuber <dnkuber2002@gmail.com>
2022-09-22 15:03:16 +0530

    after adding cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const userRouter = require('./routes/userRouter');
 
@@ -9,11 +10,12 @@ const app = express();
 
 // Use middleware
 app.use(express.json());
+app.use(cors());
 
 // Routes
 app.use('/api/users', userRouter);
 
-// Mongodb connect 
+// Mongodb connect
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
